9f7c74246a8c997991c9af384402fee163b7094e
Metal Finger <dev.prajwalp@gmail.com>
2025-11-28 14:31:14 +0530

    driver class

---
+++ src/expense_income_tracker/Expense_Income_Tracker.java
@@ -0,0 +1,12 @@
+// ExpenseIncomeEntry.java
+// Plain data object (POJO).
+// Holds date (String), description (String), amount (double) and type (String).
+// Has getters/setters and a constructor.
+
+package expense_income_tracker;
+
+public class Expense_Income_Tracker {
+    public static void main(String[] args) {
+        new ExpensesIncomesTracker().setLocationRelativeTo(null);
+    }
+}
